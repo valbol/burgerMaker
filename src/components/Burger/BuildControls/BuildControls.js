9648c3510318092a8a85cf3cbccde35ced0bf8e1
VALERYBOLTAVSKY <valerybo@il.ibm.com>
2020-05-30 18:01:54 +0300

    Dispaly burger price , adding the order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
+
 const controls = [
 
     {label: 'Salad', type: 'salad'},
     {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'chess'},
+    {label: 'Cheese', type: 'cheese'},
     {label: 'Meat', type: 'meat'}
 ];
 
 const BuildControls = (porps) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{porps.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => porps.ingredientAdded(ctrl.type)}
+                removed={() => porps.ingredientRemoved(ctrl.type)}
+                disabled={porps.disabled[ctrl.type]} />
         ))}
+        <button 
+        className={classes.OrderButton}
+        disabled={!porps.purchasable}>ORDER NOW</button>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,15 @@ import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+
+}
+
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
@@ -15,14 +24,73 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4,
+        purchasable: false
     }
+    updatePurchaseState (ingredients) {
+       
+        // const sum = Object.values(ingredients).reduce((sum, el) => {
+        //     return sum + el;
+        // }, 0);
+
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        })
+        .reduce((sum, el) => {
+           return sum + el ;
+        }, 0);
+        this.setState({purchasable: sum > 0});
+
+    }
+    addIngredientHendler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
+
+    };
+    removeIngredientHendler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0){
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
+    };
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for(let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+        // {salad: true, meat: false, ...}
         return (
             <Auxi>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />              
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHendler}
+                    ingredientRemoved={this.removeIngredientHendler}    
+                    disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
+                    price={this.state.totalPrice} />           
             </Auxi>
         );
     }
